a01f65990659ad3b0c3a006e74a617e258e8770e
codingcool21 <bryan.quadras2001@gmail.com>
2015-01-26 17:17:53 +0400

    Added Vex modals, added logout button and fixed small isues

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 var $app = {
+    createDialogWithHTML: function (html) {
+        vex.open({
+            content: html
+        });
+    },
     buttonRoutingAndChangingView: function (button) {
         $("#nav-system").find("li").removeClass("active");
         $("#nav-system").find("[data-name='" + button + "']").toggleClass("active");
@@ -24,16 +29,24 @@ var $app = {
     }
 };
 if (localStorage.getItem("username")) {
-    $app.loggedIn = true;
-$app.firebaseref.child("users/" + localStorage.getItem("username") + "/name").on("value", function (n) {
-    $("#statusBar").text("Welcome back, " + n.val() + ". Firebase is now connected!");
-});
-} else {}
+    $app.firebaseref.child("users/" + localStorage.getItem("username") + "/name").on("value", function (n) {
+        $app.loggedIn = true;
+        $("#statusBar").text("Welcome back, " + n.val() + ". Firebase is now connected!");
+        $("#auth-form").hide();
+        $("#logged-in").find("#username-p").text(localStorage.getItem("fullname"));
+        $("#logged-in").show();
+    }, function(error) {
+        $("#auth_username").val(localStorage.getItem("username"));
+        $("#auth-form").on("submit", $app.authenticateToFirebase).show();
+    });
+} else {
+    $("#auth-form").on("submit", $app.authenticateToFirebase).show();
+}
 $app.voiceCommands = {
     "set username as :name": function (username) {
-       $("[name='auth_username']").val(username.toLowerCase());
+        $("[name='auth_username']").val(username.toLowerCase());
     }
-}
+};
 Path.map("#home").to(function () {
     $app.buttonRoutingAndChangingView("home");
 });
@@ -43,23 +56,29 @@ Path.map("#profile").to(function () {
 Path.map("#messages").to(function () {
     $app.buttonRoutingAndChangingView("messages");
 });
+Path.map("#logout").to(function () {
+    $app.loggedIn = undefined;
+    $app.firebaseref.unauth();
+    location.href = "#home";
+    location.reload();
+});
 Path.root("#home");
 
 //annyang.addCommands($app.voiceCommands);
-function Start(app) {
+function start(app) {
     //app.authenticateToFirebase();
 }
 $(function () {
- //   Path.history.pushState({}, "", location.hash);
-  //  annyang.start();
+    //   Path.history.pushState({}, "", location.hash);
+    //  annyang.start();
     Path.listen();
     //$.get("auth.html", function (data) {
     //    $(data).appendTo("body");
     //});
-    Start($app);
+    start($app);
     //$app.centerElementOnPage("#auth-form", $("#auth-form").innerWidth(), "px", "left");
     //alert();
-    $(window).resize(function() {
-    //$(".navbar-btns").find("a").height($("#navbar").innerHeight());
+    $(window).resize(function () {
+        //$(".navbar-btns").find("a").height($("#navbar").innerHeight());
     });
 });
--- auth.js
+++ auth.js
@@ -1,34 +1,42 @@
+var $ = window.jQuery,
+    $app = window.$app,
+    CryptoJS = window.CryptoJS,
+    FirebaseTokenGenerator = window.FirebaseTokenGenerator;
+
 function activateLogIn() {
     $app.authenticateToFirebase = function (event) {
         event.preventDefault();
         localStorage.setItem("username", $("#auth-form").find("[name='auth_username']").val());
-        var username = localStorage.getItem("username");
-        var password = $("#auth-form").find("[name='auth_password']").val();
+        var username = localStorage.getItem("username"),
+            password = $("#auth-form").find("[name='auth_password']").val();
         $app.firebaseref.child("users/" + username + "/authkey").on("value", function (data) {
-            var d = CryptoJS.AES.decrypt(data.val(), password).toString(CryptoJS.enc.Utf8);
-            var FTG = new FirebaseTokenGenerator(d);
-            var t = FTG.createToken({
-                uid: "#" + username,
-                userid: username,
-                user: true
-            }, function (e) {
-                $("#auth-form").show();
-                //$("#statusBar").text("Please log in to see your checklists");
-            });
+            var d = CryptoJS.AES.decrypt(data.val(), password).toString(CryptoJS.enc.Utf8),
+                FTG = new FirebaseTokenGenerator(d),
+                t = FTG.createToken({
+                    uid: "#" + username,
+                    userid: username,
+                    user: true
+                });
             $app.firebaseref.authWithCustomToken(t, function (error, authData) {
-                if ($app.loggedIn == undefined) {
+                if ($app.loggedIn === undefined) {
                     $app.firebaseref.child("users/" + username + "/name").on("value", function (n) {
                         $("#statusBar").text("Welcome back, " + n.val() + ". Firebase is now connected!");
+                        $("#auth-form").find("input").val("");
+                        $("#auth-form").hide();
+                        $("#logged-in").show();
+                        $app.username = n.val();
+                        localStorage.setItem("fullname", n.val());
+                        $("#username-p").text(n.val());
+                        $app.loggedIn = true;
                     });
-                    $app.loggedIn = true;
                 } else {
                     return false;
                 }
             });
         });
-    }
-    $("#auth-form").on("submit", $app.authenticateToFirebase).show();
+    };
     
+
 }
 //var parent = parent.document.getElementById("auth");
 $(activateLogIn);
